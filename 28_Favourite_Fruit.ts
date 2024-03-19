3c914a9635f88a2fb791ff39b1dcc13cad59043c
Rose-Github-Account <162344834+Rose-Github-Account@users.noreply.github.com>
2024-03-19 22:03:56 +0500

    Add files via upload

---
+++ 28_Favourite_Fruit.ts
@@ -0,0 +1,23 @@
+
+//Make a array of your three favorite fruits and call it favorite_fruits.
+let favourite_fruits :string[] = ['apple','mango','banana'];
+
+if(favourite_fruits.includes('apple')){
+    console.log('I really like apple!');
+}
+
+if(favourite_fruits.includes('banana')){
+    console.log('I really like banana!');
+}
+
+if(favourite_fruits.includes('mango')){
+    console.log('I really like mango!');
+}
+
+if(favourite_fruits.includes('strawberry')){
+    console.log('I really like strawberry!');
+}
+
+if(favourite_fruits.includes('orange')){
+    console.log('I really like orange!');
+}
