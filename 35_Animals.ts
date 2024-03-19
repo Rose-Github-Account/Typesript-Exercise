731d1f0113f045372f2e3fc1fccb53672a52e80b
Rose-Github-Account <162344834+Rose-Github-Account@users.noreply.github.com>
2024-03-19 22:12:01 +0500

    Add files via upload

---
+++ 35_Animals.ts
@@ -0,0 +1,8 @@
+//storing animals that have same characteristics in an array
+let Animals :string[] =['dog','cat','rabbit'];
+
+//using for off loop
+for(let Animal of Animals){
+    console.log(`A ${Animal} dog would make a great pet.`);
+}
+console.log(`Any of these animals would make a great pet!.`);
