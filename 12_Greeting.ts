5677b8f6eb45522961f411520d33a451517baadf
Rose-Github-Account <162344834+Rose-Github-Account@users.noreply.github.com>
2024-03-19 21:50:30 +0500

    Add files via upload

---
+++ 12_Greeting.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['Khola','Memona','Mehak','Ayesha'] ;
+
+const message :string ='Welcome to typescript class: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i])
+
+}
