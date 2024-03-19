b29f0ab940e624b07c6620e7335ea1495f6130bb
Rose-Github-Account <162344834+Rose-Github-Account@users.noreply.github.com>
2024-03-19 22:14:35 +0500

    Add files via upload

---
+++ 39_City_Names.ts
@@ -0,0 +1,13 @@
+
+//making function with return value
+function city_country(city :string, country :string){
+    return `${city},${country}`;
+}
+
+//now calling the function as console.log()
+console.log(city_country('Karachi','Pakistan'));
+
+console.log(city_country('Tokyo','Japan'));
+
+console.log(city_country('Paris','France'));
+  
