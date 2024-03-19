f0dca60b90143a96f93ecfe523fcf9c2dc5b7be9
Rose-Github-Account <162344834+Rose-Github-Account@users.noreply.github.com>
2024-03-19 22:11:21 +0500

    Add files via upload

---
+++ 34_Pizzas.ts
@@ -0,0 +1,9 @@
+
+// storing three different pizzas
+let Pizzas :string[] =['Pepperoni','cuban','BBQ chicken'];
+
+//using for off loop
+for(let Pizza of Pizzas){
+    console.log(`I like ${Pizza} pizza.`)
+}
+console.log('I really love pizza!');
