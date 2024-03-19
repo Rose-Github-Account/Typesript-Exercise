638b6ee2f849cb6e485c80c9654eebfeef3f1bf4
Rose-Github-Account <162344834+Rose-Github-Account@users.noreply.github.com>
2024-03-19 22:17:04 +0500

    Add files via upload

---
+++ 43_Unchanged_Magicians.ts
@@ -0,0 +1,33 @@
+
+
+//storing the magicians name in array
+let magician :string[] =['Harry potter','Hermione Granger','Ron Weasley','Albus Dumbledore'];
+
+function copy_array (arr:string[]){
+    return [...arr]
+}
+
+
+function make_great ( magicianArry:string[] ){
+    for(let i=0;i<magicianArry.length;i++){
+      
+        magicianArry[i]= 'The Great ' + magicianArry[i]
+
+    }
+}
+
+function show_magicians(magicians :string[]){
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+
+const copyMagicianArray= copy_array(magician);
+
+make_great(copyMagicianArray);
+
+console.log('\n\nThis is my original array:')
+show_magicians(magician);
+
+console.log('\n\nThis is my modified copy of the array:')
+show_magicians(copyMagicianArray);
