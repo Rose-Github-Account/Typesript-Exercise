b3a594088c949f7224c9bbb8615594214936c7d7
Rose-Github-Account <162344834+Rose-Github-Account@users.noreply.github.com>
2024-03-19 22:09:23 +0500

    Add files via upload

---
+++ 31_No_Users.ts
@@ -0,0 +1,11 @@
+
+
+let userNames :string[] = ['admin','Junaid','Zeeshan','Kamran','Daniyal'];
+
+if(userNames.length === 0){
+    console.log('We need to find some users!')
+}
+else{
+    userNames = [];
+    console.log('All usernames have been removed. ' + userNames.length)
+}
