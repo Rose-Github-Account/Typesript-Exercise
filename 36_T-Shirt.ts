f3e0f99ce5d9c5724c4c3d4b34657890557df4b3
Rose-Github-Account <162344834+Rose-Github-Account@users.noreply.github.com>
2024-03-19 22:12:55 +0500

    Add files via upload

---
+++ 36_T-Shirt.ts
@@ -0,0 +1,10 @@
+
+//call the function.
+function make_shirt(size :string,text :string){
+    console.log(`Creating a ${size} shirt with the message: ${text}`)
+}
+
+//storing the arguements to call the function.
+make_shirt('large','Hello World');
+make_shirt('medium','Hello World');
+make_shirt('small','Hello World'); 
